ad307cbbdd24ebe0687853ea25b85e6786c2f261
joey00072 <shubhamchoudhari00072@gmail.com>
2020-12-22 15:58:04 +0530

    #untill 22-12-2020

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Login from "./components/login";
